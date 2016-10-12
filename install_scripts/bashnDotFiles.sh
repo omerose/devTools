b6d9d2c89d09e135b588f31526dc8d9ed6ea8571
Omer Dobrescu <omer.dobrescu@tiko3d.com>
2016-10-12 12:22:59 -0400

    Add git commit template
    Allows developers to more easily follow our guidelines.

--- install_scripts/bashnDotFiles.sh
+++ install_scripts/bashnDotFiles.sh
@@ -12,6 +12,7 @@ ln -s ~/devTools/symlinks/dotfiles/.aliases ~
 ln -s ~/devTools/symlinks/dotfiles/.bashrc ~
 ln -s ~/devTools/symlinks/dotfiles/.gitconfig ~
 ln -s ~/devTools/symlinks/dotfiles/.gitignore_global ~
+ln -s ~/devTools/symlinks/dotfiles/.gitmessage.txt ~
 ln -s ~/devTools/symlinks/dotfiles/.ideavimrc ~
 ln -s ~/devTools/symlinks/dotfiles/.tmux.conf ~
 ln -s ~/devTools/symlinks/dotfiles/.vimrc ~
