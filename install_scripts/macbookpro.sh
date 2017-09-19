be0119eea838690dc696d495920a1a926b41766b
Omer Dobrescu <omerdobrescu@github.com>
2017-09-19 18:57:53 -0400

    Add reminder to install vscode settings

--- install_scripts/macbookpro.sh
+++ install_scripts/macbookpro.sh
@@ -2,7 +2,7 @@
 xargs brew install < brew.installed
 
 
-# Don't forget to install rescuetime, flux, chrome, vscode, and ohmyzsh?, iterm2, install new version of vim (maybe also create alias vim to point to new version)
+# Don't forget to install rescuetime, flux, chrome, vscode (run vscodesettings.sh after install), and ohmyzsh?, iterm2, install new version of vim (maybe also create alias vim to point to new version)
 
 # when installing iterm2:
 # on iterm2, go to profile, settings, colors, put solarized dark, change background to black, and set font as 'Menlo 14pt in iTerm 2'
